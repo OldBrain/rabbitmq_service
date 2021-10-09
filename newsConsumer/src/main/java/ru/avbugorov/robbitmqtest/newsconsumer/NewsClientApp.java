5985ca82b9e872553734ab53311f4d4d61c62247
OldBrain <hell_brain@mail.ru>
2021-10-10 02:28:09 +0400

    dependencies added

---
+++ newsConsumer/src/main/java/ru/avbugorov/robbitmqtest/newsconsumer/NewsClientApp.java
@@ -0,0 +1,11 @@
+package ru.avbugorov.robbitmqtest.newsconsumer;
+
+import ru.avbugorov.robbitmqtest.domain.newsconnection.NewsConnection;
+
+public class NewsClientApp {
+    private NewsConnection newsConnection = new NewsConnection();
+
+    public static void main(String[] args) {
+
+    }
+}
